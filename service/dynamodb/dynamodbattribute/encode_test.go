mdonnellyli/aws-sdk-go

service/dynamodb/dynamodbattribute: Improve AttributeValue marshaling
This change deprecates the `ConvertTo` and `ConvertFrom` functions. In
there place a Marshal and Unmarshal functions were added. The new
functions also include Map and List aliases similar to the Convert
functions for convenience.

The new `Marshal` and `Unmarshal` fully support DynamoDB AttributeValue
types, without relying on `encoding/json` for converting typed values to
`map[string]interface{}`. This has the added benefit of being able to
(un)marshal Binary, Number, and String sets.

This change does not modify the existing Convert functions. Just
deprecating them. It became too complex to update the Convert
functions while maintaining backward's capability with all of
the possible reliance on `encoding/json`.

Fix #529

Also fixes a bug in `awsutil.Prettify ` paniciting on invalid values.

service/dynamodb/dynamodbattribute/encode_test.go
@@ -1,0 +1,121 @@
+package dynamodbattribute
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMarshalErrorTypes(t *testing.T) {
+	var _ awserr.Error = (*InvalidMarshalError)(nil)
+	var _ awserr.Error = (*unsupportedMarshalTypeError)(nil)
+}
+
+func TestMarshalShared(t *testing.T) {
+	for i, c := range sharedTestCases {
+		av, err := Marshal(c.expected)
+		assertConvertTest(t, i, av, c.in, err, c.err)
+	}
+}
+
+func TestMarshalListShared(t *testing.T) {
+	for i, c := range sharedListTestCases {
+		av, err := MarshalList(c.expected)
+		assertConvertTest(t, i, av, c.in, err, c.err)
+	}
+}
+
+func TestMarshalMapShared(t *testing.T) {
+	for i, c := range sharedMapTestCases {
+		av, err := MarshalMap(c.expected)
+		assertConvertTest(t, i, av, c.in, err, c.err)
+	}
+}
+
+type marshalMarshaler struct {
+	Value  string
+	Value2 int
+	Value3 bool
+}
+
+func (m *marshalMarshaler) MarshalDynamoDBAttributeValue(av *dynamodb.AttributeValue) error {
+	av.M = map[string]*dynamodb.AttributeValue{
+		"abc": &dynamodb.AttributeValue{S: &m.Value},
+		"def": &dynamodb.AttributeValue{N: aws.String(fmt.Sprintf("%d", m.Value2))},
+		"ghi": &dynamodb.AttributeValue{BOOL: &m.Value3},
+	}
+
+	return nil
+}
+
+func TestMarshalMashaler(t *testing.T) {
+	m := &marshalMarshaler{
+		Value:  "value",
+		Value2: 123,
+		Value3: true,
+	}
+
+	expect := &dynamodb.AttributeValue{
+		M: map[string]*dynamodb.AttributeValue{
+			"abc": &dynamodb.AttributeValue{S: aws.String("value")},
+			"def": &dynamodb.AttributeValue{N: aws.String("123")},
+			"ghi": &dynamodb.AttributeValue{BOOL: aws.Bool(true)},
+		},
+	}
+
+	actual, err := Marshal(m)
+	assert.NoError(t, err)
+
+	assert.Equal(t, expect, actual)
+}
+
+type testOmitEmptyElemListStruct struct {
+	Values []string `dynamodbav:",omitemptyelem"`
+}
+
+type testOmitEmptyElemMapStruct struct {
+	Values map[string]interface{} `dynamodbav:",omitemptyelem"`
+}
+
+func testMarshalListOmitEmptyElem(t *testing.T) {
+	expect := &dynamodb.AttributeValue{
+		M: map[string]*dynamodb.AttributeValue{
+			"Values": &dynamodb.AttributeValue{L: []*dynamodb.AttributeValue{
+				{S: aws.String("abc")},
+				{S: aws.String("123")},
+			}},
+		},
+	}
+
+	m := testOmitEmptyElemListStruct{Values: []string{"abc", "", "123"}}
+
+	actual, err := Marshal(m)
+	assert.NoError(t, err)
+	assert.Equal(t, expect, actual)
+}
+
+func testMarshalMapOmitEmptyElem(t *testing.T) {
+	expect := &dynamodb.AttributeValue{
+		M: map[string]*dynamodb.AttributeValue{
+			"Values": &dynamodb.AttributeValue{M: map[string]*dynamodb.AttributeValue{
+				"abc": {N: aws.String("123")},
+				"klm": {S: aws.String("abc")},
+			}},
+		},
+	}
+
+	m := testOmitEmptyElemMapStruct{Values: map[string]interface{}{
+		"abc": 123,
+		"efg": nil,
+		"hij": "",
+		"klm": "abc",
+	}}
+
+	actual, err := Marshal(m)
+	assert.NoError(t, err)
+	assert.Equal(t, expect, actual)
+}

yiluncui-zz/awssdkgotest

service/dynamodb/dynamodbattribute/encode_test.go
@@ -1,0 +1,121 @@
+package dynamodbattribute
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMarshalErrorTypes(t *testing.T) {
+	var _ awserr.Error = (*InvalidMarshalError)(nil)
+	var _ awserr.Error = (*unsupportedMarshalTypeError)(nil)
+}
+
+func TestMarshalShared(t *testing.T) {
+	for i, c := range sharedTestCases {
+		av, err := Marshal(c.expected)
+		assertConvertTest(t, i, av, c.in, err, c.err)
+	}
+}
+
+func TestMarshalListShared(t *testing.T) {
+	for i, c := range sharedListTestCases {
+		av, err := MarshalList(c.expected)
+		assertConvertTest(t, i, av, c.in, err, c.err)
+	}
+}
+
+func TestMarshalMapShared(t *testing.T) {
+	for i, c := range sharedMapTestCases {
+		av, err := MarshalMap(c.expected)
+		assertConvertTest(t, i, av, c.in, err, c.err)
+	}
+}
+
+type marshalMarshaler struct {
+	Value  string
+	Value2 int
+	Value3 bool
+}
+
+func (m *marshalMarshaler) MarshalDynamoDBAttributeValue(av *dynamodb.AttributeValue) error {
+	av.M = map[string]*dynamodb.AttributeValue{
+		"abc": &dynamodb.AttributeValue{S: &m.Value},
+		"def": &dynamodb.AttributeValue{N: aws.String(fmt.Sprintf("%d", m.Value2))},
+		"ghi": &dynamodb.AttributeValue{BOOL: &m.Value3},
+	}
+
+	return nil
+}
+
+func TestMarshalMashaler(t *testing.T) {
+	m := &marshalMarshaler{
+		Value:  "value",
+		Value2: 123,
+		Value3: true,
+	}
+
+	expect := &dynamodb.AttributeValue{
+		M: map[string]*dynamodb.AttributeValue{
+			"abc": &dynamodb.AttributeValue{S: aws.String("value")},
+			"def": &dynamodb.AttributeValue{N: aws.String("123")},
+			"ghi": &dynamodb.AttributeValue{BOOL: aws.Bool(true)},
+		},
+	}
+
+	actual, err := Marshal(m)
+	assert.NoError(t, err)
+
+	assert.Equal(t, expect, actual)
+}
+
+type testOmitEmptyElemListStruct struct {
+	Values []string `dynamodbav:",omitemptyelem"`
+}
+
+type testOmitEmptyElemMapStruct struct {
+	Values map[string]interface{} `dynamodbav:",omitemptyelem"`
+}
+
+func TestMarshalListOmitEmptyElem(t *testing.T) {
+	expect := &dynamodb.AttributeValue{
+		M: map[string]*dynamodb.AttributeValue{
+			"Values": &dynamodb.AttributeValue{L: []*dynamodb.AttributeValue{
+				{S: aws.String("abc")},
+				{S: aws.String("123")},
+			}},
+		},
+	}
+
+	m := testOmitEmptyElemListStruct{Values: []string{"abc", "", "123"}}
+
+	actual, err := Marshal(m)
+	assert.NoError(t, err)
+	assert.Equal(t, expect, actual)
+}
+
+func TestMarshalMapOmitEmptyElem(t *testing.T) {
+	expect := &dynamodb.AttributeValue{
+		M: map[string]*dynamodb.AttributeValue{
+			"Values": &dynamodb.AttributeValue{M: map[string]*dynamodb.AttributeValue{
+				"abc": {N: aws.String("123")},
+				"klm": {S: aws.String("abc")},
+			}},
+		},
+	}
+
+	m := testOmitEmptyElemMapStruct{Values: map[string]interface{}{
+		"abc": 123.,
+		"efg": nil,
+		"hij": "",
+		"klm": "abc",
+	}}
+
+	actual, err := Marshal(m)
+	assert.NoError(t, err)
+	assert.Equal(t, expect, actual)
+}